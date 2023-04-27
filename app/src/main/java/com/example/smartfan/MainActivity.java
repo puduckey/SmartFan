02852d38acb455be07a392efeadaae0bd7972d10
puduckey <104123662+puduckey@users.noreply.github.com>
2023-04-27 05:22:18 -0400

    mainactivity e inicio

--- app/src/main/java/com/example/smartfan/MainActivity.java
+++ app/src/main/java/com/example/smartfan/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.smartfan;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Aquí iniciamos la actividad siguiente
+                Intent intent = new Intent(MainActivity.this, Inicio.class);
+                startActivity(intent);
+                finish(); // para que el usuario no pueda volver a esta actividad presionando el botón "Atrás"
+            }
+        }, 3000); // 3000 milisegundos = 3 segundos
     }
 }
