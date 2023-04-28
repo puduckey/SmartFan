decd38f2ad39fca8ac95b61dc7d1145925c8978e
puduckey <104123662+puduckey@users.noreply.github.com>
2023-04-27 20:51:09 -0400

    botones inicio

--- app/src/main/java/com/example/smartfan/Inicio.java
+++ app/src/main/java/com/example/smartfan/Inicio.java
@@ -1,7 +1,9 @@
 package com.example.smartfan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void irPantallaConfiguracion(View v){
+        Intent i = new Intent(Inicio.this, Configuracion.class);
+        startActivity(i);
+    }
+
+    public void irPantallaEstadisticas(View v){
+        Intent i = new Intent(Inicio.this, estadisticas.class);
+        startActivity(i);
     }
 }
